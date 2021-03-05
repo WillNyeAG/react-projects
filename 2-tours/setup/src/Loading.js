b2bd5794c232a5041a288b21892722d3a30c2ad2
William AG <antoniogutie@wis.edu>
2021-03-04 23:36:18 -0800

    Main Tours project functionality finalized

--- 2-tours/setup/src/Loading.js
+++ 2-tours/setup/src/Loading.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Loading = () => {
   return (
     <div className="loading">
+      {/*TODO:  Animate, spinner, three dots, etc */}
       <h1>loading...</h1>
     </div>
   );
