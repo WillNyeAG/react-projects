62e2cbe82f7a17652d0248996bb09d0970894ad8
William AG <antoniogutie@wis.edu>
2021-03-04 22:06:23 -0800

    Added TODO for future functionality. Will comeback in a few weeks with more experience and complete

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -5,6 +5,8 @@ function App() {
   const [people, setPeople] = useState(data)
   return <main>
     <section className='container'>
+      {/*TODO: Add sorting buttons for age, name, set a current date and add a birthdate to each person. 
+      Show birthdays closest to current date within custom range. */}
       <h3>{people.length} birthdays today</h3>
       <List people={people}/>
       <button onClick={() => setPeople([])}>clear all</button>
