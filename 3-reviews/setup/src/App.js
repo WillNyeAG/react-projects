b324c9df2ffa4419ce9b3338984c396173fe6f82
William AG <antoniogutie@wis.edu>
2021-03-06 19:03:30 -0800

    4- accordion project finished, animations pending

--- 3-reviews/setup/src/App.js
+++ 3-reviews/setup/src/App.js
@@ -13,5 +13,5 @@ function App() {
     </main>
   );
 }
-
+/* TODO: Add animations when switching, make more design stability for different text sizes(jumps around too muich when a  lot of text is in info), auto scrolling for 1 s*/
 export default App;
