b5fa15acfb6316379be7230f251b102bd149442a
William AG <antoniogutie@wis.edu>
2021-03-11 20:13:54 -0800

    color generator started. color list created from input

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -4,7 +4,43 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+      console.log(colors)
+    }
+    catch (error) {
+      setError(true);
+      console.log("Unable to parse color");
+    }
+
+  }
+
+  return (
+    <>
+      <section className='container'>
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input type='text' value={color} onChange={(e) => setColor(e.target.value)} placeholder='#03a9fc' className={`${error ? 'error' : null}`} />
+          <button className='btn' type='submit'>submit</button>
+        </form>
+      </section>
+      <section className='colors'>
+        {list.map((color, index) => {
+          console.log(color)
+          return <SingleColor key={index} {...color} index={index} />
+        })}
+      </section>
+    </>
+  );
 }
 
 export default App
+// TODO: if no input at beginning, rainbow shift thru color
+// Add color picker for friendly UI option
